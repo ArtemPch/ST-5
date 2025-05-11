7aeda34cab3505c70189abd438b27472f888db5b
ArtemPch <artemon33005022@yandex.ru>
2025-05-11 18:34:26 +0300

    commit 6

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -4,63 +4,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class AppTest {
+class AppTest {
     @Test
-    public void average1() {
+    void testAverage1() {
         Sqrt sqrt = new Sqrt(1);
         assertEquals(15, sqrt.average(10, 20));
     }
 
     @Test
-    public void average2() {
+    void testAverage2() {
         Sqrt sqrt = new Sqrt(1);
         assertEquals(30, sqrt.average(20, 40));
     }
 
     @Test
-    public void good1() {
+    void testGood1() {
         Sqrt sqrt = new Sqrt(1);
         assertTrue(sqrt.good(9, 81));
     }
 
     @Test
-    public void good2() {
+    void testGood2() {
         Sqrt sqrt = new Sqrt(1);
         assertTrue(sqrt.good(8.99999999999999, 81));
     }
 
     @Test
-    public void good3() {
+    void testGood3() {
         Sqrt sqrt = new Sqrt(1);
         assertFalse(sqrt.good(8.5, 81));
     }
 
     @Test
-    public void improve1() {
+    void testImprove1() {
         Sqrt sqrt = new Sqrt(1);
         assertEquals(4.5625, sqrt.improve(8, 9));
     }
 
     @Test
-    public void improve2() {
+    void testImprove2() {
         Sqrt sqrt = new Sqrt(1);
         assertEquals(5.45, sqrt.improve(10, 9));
     }
 
     @Test
-    public void iter() {
+    void testIter() {
         Sqrt sqrt = new Sqrt(1);
         assertEquals(3.0, sqrt.iter(5, 9), 0.0000001);
     }
 
     @Test
-    public void calc1() {
+    void testCalc1() {
         Sqrt sqrt = new Sqrt(9);
         assertEquals(3.0, sqrt.calc(), 0.0000001);
     }
 
     @Test
-    public void calc2() {
+    void testCalc2() {
         Sqrt sqrt = new Sqrt(81);
         assertEquals(9.0, sqrt.calc(), 0.0000001);
     }
