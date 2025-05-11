f5aa1f6a18eedde437f4c251134d5f359188aaa4
ArtemPch <artemon33005022@yandex.ru>
2025-05-11 18:02:59 +0300

    commit 4

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,8 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args)
+public class App
+{
+   public static void main(String[] args)
    {
       double val = Double.parseDouble("2.0");
       Sqrt sqrt = new Sqrt(val);
